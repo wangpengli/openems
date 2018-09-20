7e3d05567d118efa6df0242280bc1e088ac9dea8
KyleMicrocare <kyle@microcare.co.za>
2018-09-20 13:39:34 +0200

    Moved serial port handling to MCCommsPacketBuffer; improved concurrency implementation

--- io.openems.edge.bridge.mc-comms/src/io/openems/edge/bridge/mc_comms/util/MCCommsPacketBuffer.java
+++ io.openems.edge.bridge.mc-comms/src/io/openems/edge/bridge/mc_comms/util/MCCommsPacketBuffer.java
@@ -1,5 +1,7 @@
 package io.openems.edge.bridge.mc_comms.util;
 
+import com.fazecast.jSerialComm.SerialPort;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -9,34 +11,55 @@ import java.util.concurrent.*;
 
 public class MCCommsPacketBuffer {
 
-    private LinkedBlockingDeque<TimedByte> timedByteQueue = new LinkedBlockingDeque<>();
-    private ConcurrentSkipListMap<Long, MCCommsPacket> RXPacketQueue = new ConcurrentSkipListMap<>();
-    public ConcurrentLinkedDeque<MCCommsPacket> TXPacketQueue = new ConcurrentLinkedDeque<>();
+    //serial port
+    private SerialPort serialPort;
+    //concurrent queues for threads
+    private final LinkedBlockingDeque<TimedByte>
+            timedByteQueue = new LinkedBlockingDeque<>();
+    private final ConcurrentSkipListMap<Long, MCCommsPacket>
+            RXPacketQueue = new ConcurrentSkipListMap<>();
+    private final ConcurrentLinkedDeque<MCCommsPacket>
+            TXPacketQueue = new ConcurrentLinkedDeque<>();
+    private final ConcurrentHashMap<Integer, LinkedTransferQueue<MCCommsPacket>>
+            transferQueues = new ConcurrentHashMap<>();
+    //thread executor
+    private final Executor executor = Executors.newFixedThreadPool(3);
+    //utility objects for byte/packet handling
     private PacketBuilder packetBuilder;
-    private ConcurrentHashMap<Integer, LinkedTransferQueue<MCCommsPacket>> transferQueues = new ConcurrentHashMap<>();
     private SerialByteHandler serialByteHandler;
     private PacketPicker packetPicker;
 
-    public void start(InputStream serialInputStream, OutputStream serialOutputStream) {
+    public void start(String serialPortDescriptor) {
+        //serial port
+        this.serialPort = SerialPort.getCommPort(serialPortDescriptor);
+        this.serialPort.setComPortParameters(9600, 8, 0, SerialPort.NO_PARITY);
+        this.serialPort.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
+        this.serialPort.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 0, 0);
+        this.serialPort.openPort();
         //byte reader
-        this.serialByteHandler = new SerialByteHandler(serialInputStream, serialOutputStream, timedByteQueue);
-        this.serialByteHandler.run();
+        this.serialByteHandler = new SerialByteHandler(serialPort.getInputStream(), serialPort.getOutputStream(), timedByteQueue);
+        this.executor.execute(serialByteHandler);
         //packet builder
         this.packetBuilder = new PacketBuilder(timedByteQueue, this.RXPacketQueue);
-        this.packetBuilder.run();
+        this.executor.execute(packetBuilder);
         //packet picker
         this.packetPicker = new PacketPicker();
-        this.packetPicker.run();
+        this.executor.execute(packetPicker);
     }
 
     public void stop() {
-        this.serialByteHandler.destroy();
-        this.packetBuilder.destroy();
-        this.packetPicker.destroy();
+        this.serialByteHandler.stop();
+        this.packetBuilder.stop();
+        this.packetPicker.stop();
+        this.serialPort.closePort();
     }
 
-    public void registerTransferQueue(int sourceAddress, LinkedTransferQueue<MCCommsPacket> transferQueue) {
+    void registerTransferQueue(int sourceAddress, LinkedTransferQueue<MCCommsPacket> transferQueue) {
         transferQueues.put(sourceAddress, transferQueue);
+    }
+
+    public ConcurrentLinkedDeque<MCCommsPacket> getTXPacketQueue() {
+        return TXPacketQueue;
     }
 
     private class PacketPicker extends Thread {
@@ -47,7 +70,8 @@ public class MCCommsPacketBuffer {
                         MCCommsPacket entryPacket = entry.getValue();
                         TransferQueue<MCCommsPacket> correspondingQueue = transferQueues.get(entryPacket.sourceAddress);
                         if (correspondingQueue != null) {
-                            correspondingQueue.tryTransfer(entryPacket, 5, TimeUnit.MILLISECONDS); //timeout on trying to give packets to packet handlers to prevent packet queue from filling up too fast
+                            //timeout on trying to give packets to tasks to prevent packet queue from filling up too fast
+                            correspondingQueue.tryTransfer(entryPacket, 5, TimeUnit.MILLISECONDS);
                         }
                     }
                     packetBuilder.wait(); //waits on packet builder to actually produce packets
@@ -74,53 +98,68 @@ public class MCCommsPacketBuffer {
             byte currentByte;
             long currentByteTime;
             long previousByteTime;
+            long packetStartTime;
+            long byteTimeDelta;
             TimedByte polledTimedByte;
 
             //forever loop
             while (true) {
-                polledTimedByte = timedByteQueue.pollLast(); //blocking deque will block until a value is present in the deque PERFORMANCE GAINZ!!1!
-                currentByte = polledTimedByte.getValue();
-                currentByteTime = polledTimedByte.getTime();
-                boolean endByteReceived = false;
-                if (currentByte == 83) { //don't start constructing packets until start character 'S' is received
-                    long packetStartTime = currentByteTime;
-                    long byteTimeDelta = 0;
-                    //byte consumer loop
-                    while ((currentByteTime - packetStartTime) < 35000000L && packetBuffer.position() < 24) {
-                        //if packet time window has not closed and packet byte buffer is not yet full
-                        if (endByteReceived && (byteTimeDelta > 3000000L)){
-                            //if endByte has been received and a pause of more than 3ms has elapsed...
-                            packetBuffer.put(currentByte); //... add endByte to buffer
-                            break; //... and break out of byte consumer loop
-                        } else {
-                            endByteReceived = false; //if payload byte is coincidentally 'E', prevent packet truncation
-                        }
-                        //put byte in buffer, record byte rx time
+                //blocking deque will block until a value is present in the deque PERFORMANCE GAINZ!!1!
+                polledTimedByte = timedByteQueue.pollLast();
+                if (polledTimedByte != null) {
+                    currentByte = polledTimedByte.getValue();
+                    currentByteTime = polledTimedByte.getTime();
+                    boolean endByteReceived = false;
+                    if (currentByte == 83) { //don't start constructing packets until start character 'S' is received
+                        packetStartTime = currentByteTime;
                         previousByteTime = currentByteTime;
-                        packetBuffer.put(currentByte);
-                        //get next timed-byte
-                        polledTimedByte = timedByteQueue.pollLast();
-                        //record byte rx time for new byte
-                        currentByteTime = polledTimedByte.getTime();
-                        //assign byte to holder var for next loop
-                        currentByte = polledTimedByte.getValue();
-                        //get time difference between current and last byte
-                        byteTimeDelta = currentByteTime - previousByteTime;
-                        if (currentByte == 69) {
-                            endByteReceived = true; //test if packet has truly ended on next byte consumer loop
+                        //byte consumer loop
+                        while ((currentByteTime - packetStartTime) < 35000000L && packetBuffer.position() < 24) {
+                            //while packet window period (35ms) has not closed and packet byte buffer is not yet full
+                            //get time difference between current and last byte
+                            byteTimeDelta = currentByteTime - previousByteTime;
+
+                            if (endByteReceived && (byteTimeDelta > 3000000L)){
+                                //if endByte has been received and a pause of more than 3ms has elapsed...
+                                packetBuffer.put(currentByte); //... add endByte to buffer
+                                break; //... and break out of byte consumer loop
+                            } else {
+                                endByteReceived = false; //if payload byte is coincidentally 'E', prevent packet truncation
+                            }
+                            //put byte in buffer, record byte rx time
+                            previousByteTime = currentByteTime;
+                            packetBuffer.put(currentByte);
+                            //calculate time remaining in packet window
+                            long remainingPacketWindowPeriod = 35000000L - (currentByteTime - packetStartTime);
+                            //get next timed-byte
+                            try {
+                                // ...and time out polling operation if window closes
+                                polledTimedByte = timedByteQueue.pollLast(remainingPacketWindowPeriod, TimeUnit.NANOSECONDS);
+                            } catch (InterruptedException ignored) {
+                                continue;
+                            }
+                            if (polledTimedByte != null) {
+                                //record byte rx time for new byte
+                                currentByteTime = polledTimedByte.getTime();
+                                //assign byte to holder var for next loop
+                                currentByte = polledTimedByte.getValue();
+                                if (currentByte == 69) {
+                                    endByteReceived = true; //test if packet has truly ended on next byte consumer loop
+                                }
+                            }
                         }
-                    }
-                    if (packetBuffer.position() == 24) { //if the packet is long enough
-                        try {
-                            //try put a new packet in the output queue
-                            outputSkipList.put(currentByteTime, new MCCommsPacket(packetBuffer.array()));
-                            notifyAll(); //notify waiting threads that packets are available
-                        } catch (MCCommsException ignored) {
-                            //malformed packet; ignored
+                        if (packetBuffer.position() == 24) { //if the packet is long enough
+                            try {
+                                //try put a new packet in the output queue
+                                outputSkipList.put(currentByteTime, new MCCommsPacket(packetBuffer.array()));
+                                notifyAll(); //notify waiting threads that packets are available
+                            } catch (MCCommsException ignored) {
+                                //malformed packet; ignored
+                            }
                         }
+                        //reset buffer position
+                        packetBuffer.position(0);
                     }
-                    //reset buffer
-                    packetBuffer.position(0);
                 }
             }
         }
@@ -141,8 +180,15 @@ public class MCCommsPacketBuffer {
         @Override
         public void run() {
             while (true) {
-                if (!TXPacketQueue.isEmpty())
-                    TXPacketQueue.forEach(packet -> {
+                try {
+                    while (inputStream.available() > 0) {
+                        timedByteQueue.add(new TimedByte(System.nanoTime(), (byte) inputStream.read()));
+                    }
+                } catch (IOException ignored) {
+                    resume(); //ignore exceptions and keep running
+                }
+                if (!getTXPacketQueue().isEmpty())
+                    getTXPacketQueue().forEach(packet -> {
                         try {
                             outputStream.write(packet.getPacketBuffer().array());
                             outputStream.flush();
@@ -151,12 +197,9 @@ public class MCCommsPacketBuffer {
                         }
                     });
                 try {
-                    if (inputStream.available() > 0) {
-                        timedByteQueue.add(new TimedByte(System.nanoTime(), (byte) inputStream.read()));
-                    }
                     sleep(1); //prevent maxing out the CPU on serial IO reads - TODO check system resource usage, may be heavy on small systems
-                } catch (IOException | InterruptedException ignored) {
-                    resume(); //ignore exceptions and keep running
+                } catch (InterruptedException ignored) {
+                    resume();
                 }
             }
         }
